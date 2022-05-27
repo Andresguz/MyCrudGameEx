2ab276f5756d736adafdc68c832c8c075fd57357
Andresguz <andresguzguzman@gmail.com>
2022-05-27 13:01:06 -0400

    Initial commit

---
+++ MyCrudGame/Migrations/20220527051355_moneey.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyCrudGame.Migrations
+{
+    public partial class moneey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Money",
+                table: "players",
+                type: "int",
+                fixedLength: true,
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Money",
+                table: "players");
+        }
+    }
+}
